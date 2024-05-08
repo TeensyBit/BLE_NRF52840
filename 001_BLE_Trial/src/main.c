b2187b193384254edf9f22ddce3364036da2c90b
Snehal <teensydev@gmail.com>
2024-05-08 12:54:49 +0530

    Broadcast website

--- 001_BLE_Trial/src/main.c
+++ 001_BLE_Trial/src/main.c
@@ -2,8 +2,12 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/gap.h>
 
-#define DEV_NAME  "dog"
+#define DEV_NAME  "rat"
 #define DEV_NAME_SIZE   (sizeof(DEV_NAME)-1)
+
+static unsigned char url_data[] ={0x17,'/','/','a','c','a','d','e','m','y','.',
+                                 'n','o','r','d','i','c','s','e','m','i','.',
+                                 'c','o','m'};
 
 //prepare the advertising packet
 static const struct bt_data ad[] = 
@@ -14,7 +18,21 @@ static const struct bt_data ad[] =
         BT_DATA(BT_DATA_NAME_COMPLETE,DEV_NAME,DEV_NAME_SIZE),
 };
 
-int main(void)
+//prepare the advertising packet
+static const struct bt_data sd[] = 
 {
+        // Include the URL data in the scan response packet
+        BT_DATA(BT_DATA_URI, url_data, sizeof(url_data)),
+};
+
+int main(void)
+{      
+        int err = bt_enable(NULL); //Enable BT
+        err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));	  //Start Advertising
+        /*if (err) 
+        {
+                LOG_ERR("Advertising failed to start (err %d)\n", err);		
+                return -1;	
+        }*/
         return 0;
 }
